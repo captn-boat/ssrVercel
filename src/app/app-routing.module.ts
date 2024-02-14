3b7ad36e8b0c01471ea28fba4b99f05a3ca5922f
CorentinBETZ <corentin@captnboat.com>
2024-02-14 17:57:51 +0100

    instal ang universal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     initialNavigation: 'enabledBlocking',
-  })],
+})],
   exports: [RouterModule],
 })
 export class AppRoutingModule {
