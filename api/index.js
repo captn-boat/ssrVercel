e50cde658e132645809a20c078ba171b949161eb
CorentinBETZ <corentin@captnboat.com>
2024-03-15 15:52:19 +0100

    fix: test fix ssr

--- api/index.js
+++ api/index.js
@@ -1,3 +1,6 @@
-const server = require('../dist/angular-seo-course-front/server/main');
+async function loadServer() {
+  const serverModule = await import('../dist/angular-seo-course-front/server/main');
+  return serverModule.app;
+}
 
-module.exports = server.app();
+export default loadServer().then(app => app());
